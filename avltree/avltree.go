6363bd2aeb2fe27588f5dcabe947806177d1a0d1
maximka200 <152256762+maximka200@users.noreply.github.com>
2024-07-21 13:54:23 +0500

    Add files via upload

---
+++ avltree/avltree.go
@@ -0,0 +1,65 @@
+package avltree
+
+import (
+	"fmt"
+)
+
+type AVLNode struct {
+	Val    int
+	Height int
+	Left   *AVLNode
+	Right  *AVLNode
+}
+
+func (node *AVLNode) GetHeight() int {
+	if node == nil {
+		return -1
+	}
+	return node.Height
+}
+
+func (node *AVLNode) UpdateHeight() {
+	node.Height = max(node.Left.GetHeight(), node.Right.GetHeight()) + 1
+}
+
+func (node *AVLNode) GetBalance() int {
+	if node == nil {
+		return 0
+	}
+	return node.Left.GetHeight() - node.Right.GetHeight()
+}
+
+func (node *AVLNode) RightRotate() *AVLNode {
+	left := node.Left
+	t3 := left.Right
+
+	left.Right = node
+	node.Left = t3
+
+	node.UpdateHeight()
+	left.UpdateHeight()
+
+	return left
+}
+
+func RemoveDuplicates(nums []int) int {
+	last := nums[0]
+	j := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != last {
+			nums[j] = nums[i]
+			j++
+		}
+		last = nums[i]
+	}
+	return j
+}
+
+func PreOrderTrav(tree *AVLNode) {
+	if tree == nil {
+		return
+	}
+	fmt.Println(tree.Val)
+	PreOrderTrav(tree.Left)
+	PreOrderTrav(tree.Right)
+}
---
+++ avltree/go.mod
@@ -0,0 +1,3 @@
+module avltree
+
+go 1.22.4
